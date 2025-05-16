4c986d7f0d67f6f6361f34a2351fbe3c12a6c313
Sarah Savage <me@sarah-savage.com>
2025-05-16 07:43:11 -0400

    Removing variable declarations

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,20 +11,6 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-variable "do_token" {
-  type        = string
-  description = "DigitalOcean API token"
-  default     = "" # Set via environment variable DO_TOKEN
-  sensitive   = true
-}
-
-variable "do_public_key" {
-  type        = string
-  description = "SSH public key for droplet"
-  default     = "" # Set via environment variable DO_PUBLIC_KEY
-  sensitive   = true
-}
-
 resource "digitalocean_ssh_key" "default" {
   name       = "wpc-ssh-key"
   public_key = var.do_public_key
